6dfd9dfc5f61d2d4beae5ffdf175c6030ff2b9c7
superlou <lousimons@gmail.com>
2021-01-26 14:17:25 -0500

    Linted blocks_world, maybe breaking run2

--- examples/blocks_world/methods1.py
+++ examples/blocks_world/methods1.py
@@ -6,18 +6,18 @@ This file should work correctly in both Python 2.7 and Python 3.2.
 from pyhop import helpers, hop
 
 
-def status(b1,state,goal,done_state):
+def status(b1, state, goal, done_state):
     """
     A helper function used in the methods' preconditions.
     """
-    if helpers.is_done(b1,state,goal,done_state):
+    if helpers.is_done(b1, state, goal, done_state):
         return 'done'
     elif not state.clear[b1]:
         return 'inaccessible'
     elif not (b1 in goal.pos) or goal.pos[b1] == done_state:
         return 'move-to-table'
-    elif (helpers.is_done(goal.pos[b1],state,goal,done_state) and
-          state.clear[goal.pos[b1]]):
+    elif (helpers.is_done(goal.pos[b1], state, goal, done_state)
+          and state.clear[goal.pos[b1]]):
         return 'move-to-block'
     else:
         return 'waiting'
@@ -31,9 +31,8 @@ that the method is for. For example, ('pickup', b1) has a method
 get_m(state,b1), as shown below.
 """
 
-### methods for "move_blocks"
 
-def moveb_m(state,goal):
+def moveb_m(state, goal):
     """
     This method implements the following block-stacking algorithm:
     If there's a block that can be moved to its final position, then
@@ -43,69 +42,71 @@ def moveb_m(state,goal):
     to be moved.
     """
     for b1 in helpers.all(state):
-        s = status(b1,state,goal,'table')
+        s = status(b1, state, goal, 'table')
         if s == 'move-to-table':
-            return [('move_one',b1,'table'),('move_blocks',goal)]
+            return [('move_one', b1, 'table'), ('move_blocks', goal)]
         elif s == 'move-to-block':
-            return [('move_one',b1,goal.pos[b1]), ('move_blocks',goal)]
+            return [('move_one', b1, goal.pos[b1]), ('move_blocks', goal)]
         else:
             continue
     #
     # if we get here, no blocks can be moved to their final locations
     b1 = helpers.find_if(
-        lambda x: status(x,state,goal,'table') == 'waiting',
+        lambda x: status(x, state, goal, 'table') == 'waiting',
         helpers.all(state))
-    if b1 != None:
-        return [('move_one',b1,'table'), ('move_blocks',goal)]
+    if b1 is not None:
+        return [('move_one', b1, 'table'), ('move_blocks', goal)]
     #
     # if we get here, there are no blocks that need moving
     return []
+
 
 """
 declare_methods must be called once for each taskname. Below,
 'declare_methods('get',get_m)' tells Pyhop that 'get' has one method, get_m.
 Notice that 'get' is a quoted string, and get_m is the actual function.
 """
-hop.declare_methods('move_blocks',moveb_m)
+hop.declare_methods('move_blocks', moveb_m)
 
 
-### methods for "move_one"
-def move1(state,b1,dest):
+def move1(state, b1, dest):
     """
     Generate subtasks to get b1 and put it at dest.
     """
-    return [('get', b1), ('put', b1,dest)]
-hop.declare_methods('move_one',move1)
+    return [('get', b1), ('put', b1, dest)]
 
 
-### methods for "get"
-def get_m(state,b1):
+hop.declare_methods('move_one', move1)
+
+
+def get_m(state, b1):
     """
     Generate either a pickup or an unstack subtask for b1.
     """
     if state.clear[b1]:
         if state.pos[b1] == 'table':
-                return [('pickup',b1)]
+            return [('pickup', b1)]
         else:
-                return [('unstack',b1,state.pos[b1])]
+            return [('unstack', b1, state.pos[b1])]
     else:
         return False
-hop.declare_methods('get',get_m)
 
 
-### methods for "put"
-def put_m(state,b1,b2):
+hop.declare_methods('get', get_m)
+
+
+def put_m(state, b1, b2):
     """
     Generate either a putdown or a stack subtask for b1.
     b2 is b1's destination: either the table or another block.
     """
     if state.holding == b1:
         if b2 == 'table':
-                return [('putdown',b1)]
+            return [('putdown', b1)]
         else:
-                return [('stack',b1,b2)]
+            return [('stack', b1, b2)]
     else:
         return False
-hop.declare_methods('put',put_m)
 
 
+hop.declare_methods('put', put_m)
--- examples/blocks_world/run2.py
+++ examples/blocks_world/run2.py
@@ -17,13 +17,12 @@ cause backtracking.
 ****************************************
 """)
 
-
 print('')
 hop.print_operators(hop.get_operators())
 print('')
 hop.print_methods(hop.get_methods())
 
-#############     beginning of tests     ################
+# ############     beginning of tests     ################
 
 print("""
 ****************************************
@@ -39,24 +38,24 @@ state variable in the domain should have a value.
 """
 
 state1 = hop.State('state1')
-state1.pos={'a':'b', 'b':'table', 'c':'table'}
-state1.clear={'c':True, 'b':False,'a':True}
-state1.holding=False
+state1.pos = {'a': 'b', 'b': 'table', 'c': 'table'}
+state1.clear = {'c': True, 'b': False, 'a': True}
+state1.holding = False
 
 hop.print_state(state1)
 print('')
 
 print('- these should fail:')
-hop.plan(state1, [('pickup','a')], hop.get_operators(), hop.get_methods(), verbose=1)
-hop.plan(state1,[('pickup','b')], hop.get_operators(), hop.get_methods(), verbose=1)
+hop.plan(state1, [('pickup', 'a')], hop.get_operators(), hop.get_methods(), verbose=1)
+hop.plan(state1, [('pickup', 'b')], hop.get_operators(), hop.get_methods(), verbose=1)
 print('- these should succeed:')
-hop.plan(state1,[('pickup','c')], hop.get_operators(), hop.get_methods(), verbose=1)
-hop.plan(state1,[('unstack','a','b')], hop.get_operators(), hop.get_methods(), verbose=1)
-hop.plan(state1,[('get','a')], hop.get_operators(), hop.get_methods(), verbose=1)
+hop.plan(state1, [('pickup', 'c')], hop.get_operators(), hop.get_methods(), verbose=1)
+hop.plan(state1, [('unstack', 'a', 'b')], hop.get_operators(), hop.get_methods(), verbose=1)
+hop.plan(state1, [('get', 'a')], hop.get_operators(), hop.get_methods(), verbose=1)
 print('- this should fail:')
-hop.plan(state1,[('get','b')], hop.get_operators(), hop.get_methods(), verbose=1)
+hop.plan(state1, [('get', 'b')], hop.get_operators(), hop.get_methods(), verbose=1)
 print('- this should succeed:')
-hop.plan(state1,[('get','c')], hop.get_operators(), hop.get_methods(), verbose=1)
+hop.plan(state1, [('get', 'c')], hop.get_operators(), hop.get_methods(), verbose=1)
 
 print("""
 ****************************************
@@ -77,9 +76,9 @@ hand is empty.
 """
 
 goal1a = hop.Goal('goal1a')
-goal1a.pos={'c':'b', 'b':'a', 'a':'table'}
-goal1a.clear={'c':True, 'b':False, 'a':False}
-goal1a.holding=False
+goal1a.pos = {'c': 'b', 'b': 'a', 'a': 'table'}
+goal1a.clear = {'c': True, 'b': False, 'a': False}
+goal1a.holding = False
 
 hop.print_goal(goal1a)
 print('')
@@ -87,16 +86,15 @@ print('')
 print("- Define goal1b:")
 
 goal1b = hop.Goal('goal1b')
-goal1b.pos={'c':'b', 'b':'a'}
+goal1b.pos = {'c': 'b', 'b': 'a'}
 
 hop.print_goal(goal1b)
 
-### goal1b omits some of the conditions of goal1a,
-### but those conditions will need to be achieved anyway
+# goal1b omits some of the conditions of goal1a,
+# but those conditions will need to be achieved anyway
 
-
-hop.plan(state1,[('move_blocks', goal1a)], hop.get_operators(), hop.get_methods(), verbose=1)
-hop.plan(state1,[('move_blocks', goal1b)], hop.get_operators(), hop.get_methods(), verbose=1)
+hop.plan(state1, [('move_blocks', goal1a)], hop.get_operators(), hop.get_methods(), verbose=1)
+hop.plan(state1, [('move_blocks', goal1b)], hop.get_operators(), hop.get_methods(), verbose=1)
 
 print("""
 ****************************************
@@ -109,9 +107,9 @@ the same plan.
 print("- Define state 2:")
 
 state2 = hop.State('state2')
-state2.pos={'a':'c', 'b':'d', 'c':'table', 'd':'table'}
-state2.clear={'a':True, 'c':False,'b':True, 'd':False}
-state2.holding=False
+state2.pos = {'a': 'c', 'b': 'd', 'c': 'table', 'd': 'table'}
+state2.clear = {'a': True, 'c': False, 'b': True, 'd': False}
+state2.holding = False
 
 hop.print_state(state2)
 print('')
@@ -119,9 +117,9 @@ print('')
 print("- Define goal2a:")
 
 goal2a = hop.Goal('goal2a')
-goal2a.pos={'b':'c', 'a':'d', 'c':'table', 'd':'table'}
-goal2a.clear={'a':True, 'c':False,'b':True, 'd':False}
-goal2a.holding=False
+goal2a.pos = {'b': 'c', 'a': 'd', 'c': 'table', 'd': 'table'}
+goal2a.clear = {'a': True, 'c': False, 'b': True, 'd': False}
+goal2a.holding = False
 
 hop.print_goal(goal2a)
 print('')
@@ -129,18 +127,16 @@ print('')
 print("- Define goal2b:")
 
 goal2b = hop.Goal('goal2b')
-goal2b.pos={'b':'c', 'a':'d'}
+goal2b.pos = {'b': 'c', 'a': 'd'}
 
 hop.print_goal(goal2b)
 print('')
 
+# goal2b omits some of the conditions of goal2a,
+# but those conditions will need to be achieved anyway.
 
-### goal2b omits some of the conditions of goal2a,
-### but those conditions will need to be achieved anyway.
-
-hop.plan(state2,[('move_blocks', goal2a)], hop.get_operators(), hop.get_methods(), verbose=1)
-hop.plan(state2,[('move_blocks', goal2b)], hop.get_operators(), hop.get_methods(), verbose=1)
-
+hop.plan(state2, [('move_blocks', goal2a)], hop.get_operators(), hop.get_methods(), verbose=1)
+hop.plan(state2, [('move_blocks', goal2b)], hop.get_operators(), hop.get_methods(), verbose=1)
 
 print("""
 ****************************************
@@ -151,11 +147,11 @@ Test pyhop on planning problem bw_large_d from the SHOP distribution.
 print("- Define state3:")
 
 state3 = hop.State('state3')
-state3.pos = {1:12, 12:13, 13:'table', 11:10, 10:5, 5:4, 4:14, 14:15,
-              15:'table', 9:8, 8:7, 7:6, 6:'table', 19:18, 18:17, 17:16,
-              16:3, 3:2, 2:'table'}
-state3.clear = {x:False for x in range(1,20)}
-state3.clear.update({1:True, 11:True, 9:True, 19:True})
+state3.pos = {1: 12, 12: 13, 13: 'table', 11: 10, 10: 5, 5: 4, 4: 14, 14: 15,
+              15: 'table', 9: 8, 8: 7, 7: 6, 6: 'table', 19: 18, 18: 17,
+              17: 16, 16: 3, 3: 2, 2: 'table'}
+state3.clear = {x: False for x in range(1, 20)}
+state3.clear.update({1: True, 11: True, 9: True, 19: True})
 state3.holding = False
 
 hop.print_state(state3)
@@ -164,14 +160,14 @@ print('')
 print("- Define goal3:")
 
 goal3 = hop.Goal('goal3')
-goal3.pos = {15:13, 13:8, 8:9, 9:4, 4:'table', 12:2, 2:3, 3:16, 16:11, 11:7,
-             7:6, 6:'table'}
-goal3.clear = {17:True, 15:True, 12:True}
+goal3.pos = {15: 13, 13: 8, 8: 9, 9: 4, 4: 'table', 12: 2, 2: 3, 3: 16, 16: 11,
+             11: 7, 7: 6, 6: 'table'}
+goal3.clear = {17: True, 15: True, 12: True}
 
 hop.print_goal(goal3)
 print('')
 
-hop.plan(state3,[('move_blocks', goal3)], hop.get_operators(), hop.get_methods(), verbose=1)
+hop.plan(state3, [('move_blocks', goal3)], hop.get_operators(), hop.get_methods(), verbose=1)
 
 hop.print_methods(hop.get_methods())
 
@@ -180,18 +176,16 @@ the recursion depth should go up, then down, then up again.===\n""")
 
 # verbose=2 tells pyhop to print out a message at each recursion depth
 
-hop.plan(state1,[('get', 'a')], hop.get_operators(), hop.get_methods(), verbose=2)
+hop.plan(state1, [('get', 'a')], hop.get_operators(), hop.get_methods(), verbose=2)
 
 print("""\n=== This time it shouldn't backtrack.===\n""")
 
 
-hop.plan(state1,[('get', 'c')], hop.get_operators(), hop.get_methods(), verbose=2)
+hop.plan(state1, [('get', 'c')], hop.get_operators(), hop.get_methods(), verbose=2)
 
 print("""\n=== This time it should fail.===\n""")
 
-hop.plan(state1,[('get', 'b')], hop.get_operators(), hop.get_methods(), verbose=2)
-
-
+hop.plan(state1, [('get', 'b')], hop.get_operators(), hop.get_methods(), verbose=2)
 
 print("""
 ****************************************
@@ -200,10 +194,10 @@ demonstrate different levels of verbosity
 """)
 
 print('- verbosity 0:')
-hop.plan(state1,[('get','a')], hop.get_operators(), hop.get_methods(), verbose=0)
+hop.plan(state1, [('get','a')], hop.get_operators(), hop.get_methods(), verbose=0)
 print('- verbosity 1:')
-hop.plan(state1,[('get','a')], hop.get_operators(), hop.get_methods(), verbose=1)
+hop.plan(state1, [('get','a')], hop.get_operators(), hop.get_methods(), verbose=1)
 print('- verbosity 2:')
-hop.plan(state1,[('get','a')], hop.get_operators(), hop.get_methods(), verbose=2)
+hop.plan(state1, [('get','a')], hop.get_operators(), hop.get_methods(), verbose=2)
 print('- verbosity 3:')
-hop.plan(state1,[('get','a')], hop.get_operators(), hop.get_methods(), verbose=3)
+hop.plan(state1, [('get','a')], hop.get_operators(), hop.get_methods(), verbose=3)
